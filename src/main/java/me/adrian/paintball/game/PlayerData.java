69bf242e4a4c979821fcfd55abb6e28317192d35
SoyAdrianYT001 <100530918+El77373883@users.noreply.github.com>
2026-03-16 15:58:55 -0600

    Update PlayerData.java

--- src/main/java/me/adrian/paintball/game/PlayerData.java
+++ src/main/java/me/adrian/paintball/game/PlayerData.java
@@ -1,4 +1,4 @@
-import me.adrian.paintball.game.GameTeam;
+package me.adrian.paintball.game;
 
 import org.bukkit.entity.Player;
 
@@ -9,15 +9,43 @@ public class PlayerData {
     private int coins = 32;
     private boolean alive = true;
 
-    public PlayerData(Player player) { this.player = player; }
+    public PlayerData(Player player) {
+        this.player = player;
+    }
 
-    public Player getPlayer() { return player; }
-    public GameTeam getTeam() { return team; }
-    public void setTeam(GameTeam team) { this.team = team; }
-    public int getKills() { return kills; }
-    public void addKill() { kills++; }
-    public int getCoins() { return coins; }
-    public void addCoins(int c) { coins += c; }
-    public boolean isAlive() { return alive; }
-    public void setAlive(boolean alive) { this.alive = alive; }
+    public Player getPlayer() {
+        return player;
+    }
+
+    public GameTeam getTeam() {
+        return team;
+    }
+
+    public void setTeam(GameTeam team) {
+        this.team = team;
+    }
+
+    public int getKills() {
+        return kills;
+    }
+
+    public void addKill() {
+        kills++;
+    }
+
+    public int getCoins() {
+        return coins;
+    }
+
+    public void addCoins(int c) {
+        coins += c;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public void setAlive(boolean alive) {
+        this.alive = alive;
+    }
 }
