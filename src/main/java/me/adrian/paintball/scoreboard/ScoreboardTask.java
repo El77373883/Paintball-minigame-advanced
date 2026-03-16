528c23fe21d4e88c846e0d6029340d19538e1b80
SoyAdrianYT001 <100530918+El77373883@users.noreply.github.com>
2026-03-16 11:47:19 -0600

    Create ScoreboardTask.java

---
+++ src/main/java/me/adrian/paintball/scoreboard/ScoreboardTask.java
@@ -0,0 +1,38 @@
+package me.adrian.paintball.scoreboard;
+
+import me.adrian.paintball.game.GameManager;
+import me.adrian.paintball.game.GameManager.GameTeam;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scoreboard.*;
+
+public class ScoreboardTask extends BukkitRunnable {
+
+    private final GameManager gm;
+
+    public ScoreboardTask(GameManager gm) {
+        this.gm = gm;
+    }
+
+    @Override
+    public void run() {
+        for (Player p : Bukkit.getOnlinePlayers()) {
+            Scoreboard board = Bukkit.getScoreboardManager().getNewScoreboard();
+            Objective obj = board.registerNewObjective("paintball", "dummy", "§6§lPaintball Minigame");
+            obj.setDisplaySlot(DisplaySlot.SIDEBAR);
+
+            Score kills = obj.getScore("Kills: " + gm.getKills(p));
+            kills.setScore(3);
+
+            GameTeam team = gm.getTeam(p);
+            Score teamScore = obj.getScore("Team: " + (team != null ? team.name() : "Ninguno"));
+            teamScore.setScore(2);
+
+            Score alive = obj.getScore("Vivos: " + gm.getAlivePlayers().size());
+            alive.setScore(1);
+
+            p.setScoreboard(board);
+        }
+    }
+}
