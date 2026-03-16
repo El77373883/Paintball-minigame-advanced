ef7604a789f74fa0f45714a34463a27589b968b2
SoyAdrianYT001 <100530918+El77373883@users.noreply.github.com>
2026-03-16 11:50:15 -0600

    Update PaintballPlugin.java

--- src/main/java/me/adrian/paintball/PaintballPlugin.java
+++ src/main/java/me/adrian/paintball/PaintballPlugin.java
@@ -1,29 +1,31 @@
 package me.adrian.paintball;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.Bukkit;
 import me.adrian.paintball.game.GameManager;
 import me.adrian.paintball.listener.PaintballListener;
 import me.adrian.paintball.command.PaintballCommand;
 import me.adrian.paintball.command.PaintballAdminCommand;
 import me.adrian.paintball.scoreboard.ScoreboardTask;
-import org.bukkit.Bukkit;
 
 public class PaintballPlugin extends JavaPlugin {
 
+    private static PaintballPlugin instance;
     private GameManager gameManager;
 
     @Override
     public void onEnable() {
+        instance = this;
         this.gameManager = new GameManager();
 
         // Registrar listeners
-        getServer().getPluginManager().registerEvents(new PaintballListener(gameManager), this);
+        Bukkit.getPluginManager().registerEvents(new PaintballListener(gameManager), this);
 
         // Registrar comandos
-        this.getCommand("pa").setExecutor(new PaintballCommand(this));
-        this.getCommand("paadmin").setExecutor(new PaintballAdminCommand(this));
+        this.getCommand("pa").setExecutor(new PaintballCommand());
+        this.getCommand("paadmin").setExecutor(new PaintballAdminCommand());
 
-        // Scoreboard dinámico cada segundo
+        // Scoreboard dinámico
         Bukkit.getScheduler().runTaskTimer(this, new ScoreboardTask(gameManager), 0L, 20L);
 
         getLogger().info("Paintball Minigame hecho por soyadrianyt001 - habilitado correctamente.");
@@ -32,6 +34,10 @@ public class PaintballPlugin extends JavaPlugin {
     @Override
     public void onDisable() {
         getLogger().info("PaintballPlugin deshabilitado.");
+    }
+
+    public static PaintballPlugin getInstance() {
+        return instance;
     }
 
     public GameManager getGameManager() {
