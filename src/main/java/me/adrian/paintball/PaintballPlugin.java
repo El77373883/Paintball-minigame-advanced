18d33413f941356ddd2339f74795de630492f27e
SoyAdrianYT001 <100530918+El77373883@users.noreply.github.com>
2026-03-16 13:01:40 -0600

    Update PaintballPlugin.java

--- src/main/java/me/adrian/paintball/PaintballPlugin.java
+++ src/main/java/me/adrian/paintball/PaintballPlugin.java
@@ -3,35 +3,36 @@ package me.adrian.paintball;
 import org.bukkit.plugin.java.JavaPlugin;
 import me.adrian.paintball.game.GameManager;
 import me.adrian.paintball.listener.PaintballListener;
-import me.adrian.paintball.listener.ShopListener;
-import me.adrian.paintball.shop.ShopGUI;
+import me.adrian.paintball.command.PaintballCommand;
+import me.adrian.paintball.command.PaintballAdminCommand;
 import me.adrian.paintball.scoreboard.ScoreboardTask;
 
 public class PaintballPlugin extends JavaPlugin {
 
     private GameManager gameManager;
-    private ShopGUI shopGUI;
 
     @Override
     public void onEnable() {
+        getLogger().info("Paintball Minigame hecho por soyadrianyt001");
+        saveDefaultConfig();
+
         this.gameManager = new GameManager(this);
-        this.shopGUI = new ShopGUI(gameManager);
 
-        // Listeners
         getServer().getPluginManager().registerEvents(new PaintballListener(gameManager), this);
-        getServer().getPluginManager().registerEvents(new ShopListener(shopGUI), this);
 
-        // Scoreboard actualizado cada segundo
-        new ScoreboardTask(gameManager).runTaskTimer(this, 0L, 20L);
+        this.getCommand("pa").setExecutor(new PaintballCommand(this));
+        this.getCommand("paadmin").setExecutor(new PaintballAdminCommand(this));
 
-        getLogger().info("Paintball Minigame 1.21.11 habilitado ✅ Hecho por soyadrianyt001");
+        // Start scoreboard task
+        new ScoreboardTask(this, gameManager).runTaskTimer(this, 0L, 20L);
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("PaintballPlugin deshabilitado.");
     }
 
     public GameManager getGameManager() {
         return gameManager;
-    }
-
-    public ShopGUI getShopGUI() {
-        return shopGUI;
     }
 }
