7e4e9888bf46059fd722547715663d82182f2e47
SoyAdrianYT001 <100530918+El77373883@users.noreply.github.com>
2026-03-16 11:57:41 -0600

    Update Arena.java

--- src/main/java/me/adrian/paintball/game/Arena.java
+++ src/main/java/me/adrian/paintball/game/Arena.java
@@ -1,31 +1,95 @@
 package me.adrian.paintball.game;
 
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
-import java.util.HashMap;
-import java.util.Map;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.Material;
+import org.bukkit.Color;
+import org.bukkit.inventory.meta.LeatherArmorMeta;
+import org.bukkit.Bukkit;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Arena {
 
     private final String name;
-    private final Map<Player, GameTeam> players = new HashMap<>();
+    private Location greenSpawn;
+    private Location blueSpawn;
+
+    private final List<Player> greenTeam = new ArrayList<>();
+    private final List<Player> blueTeam = new ArrayList<>();
+    private final List<Player> alivePlayers = new ArrayList<>();
 
     public Arena(String name) {
         this.name = name;
     }
 
-    public void join(Player player, GameTeam team) {
-        players.put(player, team);
+    public String getName() { return name; }
+    public void setGreenSpawn(Location loc) { this.greenSpawn = loc; }
+    public void setBlueSpawn(Location loc) { this.blueSpawn = loc; }
+    public Location getGreenSpawn() { return greenSpawn; }
+    public Location getBlueSpawn() { return blueSpawn; }
+
+    public void addPlayer(Player p, GameManager.GameTeam team) {
+        alivePlayers.add(p);
+        switch (team) {
+            case GREEN -> {
+                greenTeam.add(p);
+                equipArmor(p, Color.GREEN);
+            }
+            case BLUE -> {
+                blueTeam.add(p);
+                equipArmor(p, Color.BLUE);
+            }
+        }
+        teleportPlayer(p, team);
     }
 
-    public void leave(Player player) {
-        players.remove(player);
+    private void teleportPlayer(Player p, GameManager.GameTeam team) {
+        Location loc = (team == GameManager.GameTeam.GREEN) ? greenSpawn : blueSpawn;
+        if (loc != null) p.teleport(loc);
     }
 
-    public Map<Player, GameTeam> getPlayers() {
-        return players;
+    private void equipArmor(Player p, Color color) {
+        ItemStack helmet = new ItemStack(Material.LEATHER_HELMET);
+        ItemStack chest = new ItemStack(Material.LEATHER_CHESTPLATE);
+        ItemStack legs = new ItemStack(Material.LEATHER_LEGGINGS);
+        ItemStack boots = new ItemStack(Material.LEATHER_BOOTS);
+
+        LeatherArmorMeta hm = (LeatherArmorMeta) helmet.getItemMeta();
+        LeatherArmorMeta cm = (LeatherArmorMeta) chest.getItemMeta();
+        LeatherArmorMeta lm = (LeatherArmorMeta) legs.getItemMeta();
+        LeatherArmorMeta bm = (LeatherArmorMeta) boots.getItemMeta();
+
+        if (hm != null && cm != null && lm != null && bm != null) {
+            hm.setColor(color);
+            cm.setColor(color);
+            lm.setColor(color);
+            bm.setColor(color);
+
+            helmet.setItemMeta(hm);
+            chest.setItemMeta(cm);
+            legs.setItemMeta(lm);
+            boots.setItemMeta(bm);
+        }
+
+        p.getInventory().setHelmet(helmet);
+        p.getInventory().setChestplate(chest);
+        p.getInventory().setLeggings(legs);
+        p.getInventory().setBoots(boots);
     }
 
-    public String getName() {
-        return name;
+    public List<Player> getGreenTeam() { return greenTeam; }
+    public List<Player> getBlueTeam() { return blueTeam; }
+    public List<Player> getAlivePlayers() { return alivePlayers; }
+
+    public void removePlayer(Player p) {
+        greenTeam.remove(p);
+        blueTeam.remove(p);
+        alivePlayers.remove(p);
+    }
+
+    public boolean isTeamAlive(GameManager.GameTeam team) {
+        return !((team == GameManager.GameTeam.GREEN ? greenTeam : blueTeam).isEmpty());
     }
 }
