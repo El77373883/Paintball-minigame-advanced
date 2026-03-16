20a7aa99667bdbbcfb746212378da1802c98d605
SoyAdrianYT001 <100530918+El77373883@users.noreply.github.com>
2026-03-16 15:36:40 -0600

    Update PaintballPlugin.java

--- src/main/java/me/adrian/paintball/PaintballPlugin.java
+++ src/main/java/me/adrian/paintball/PaintballPlugin.java
@@ -3,41 +3,21 @@ package me.adrian.paintball;
 import org.bukkit.plugin.java.JavaPlugin;
 import me.adrian.paintball.game.GameManager;
 import me.adrian.paintball.command.PaintballCommand;
-import me.adrian.paintball.listener.PaintballListener;
 import me.adrian.paintball.listener.ShopListener;
 import me.adrian.paintball.shop.ShopGUI;
 
 public class PaintballPlugin extends JavaPlugin {
 
-    private static PaintballPlugin instance;
     private GameManager gameManager;
     private ShopGUI shopGUI;
 
     @Override
     public void onEnable() {
-        instance = this;
-
-        // Inicializar GameManager y ShopGUI
         this.gameManager = new GameManager();
         this.shopGUI = new ShopGUI(gameManager);
 
-        // Registrar comandos
-        this.getCommand("paintball").setExecutor(new PaintballCommand(gameManager, shopGUI));
-
-        // Registrar listeners
-        getServer().getPluginManager().registerEvents(new PaintballListener(gameManager), this);
+        getCommand("pa").setExecutor(new PaintballCommand(this));
         getServer().getPluginManager().registerEvents(new ShopListener(shopGUI), this);
-
-        getLogger().info("PaintballPlugin habilitado correctamente.");
-    }
-
-    @Override
-    public void onDisable() {
-        getLogger().info("PaintballPlugin deshabilitado.");
-    }
-
-    public static PaintballPlugin getInstance() {
-        return instance;
     }
 
     public GameManager getGameManager() {
