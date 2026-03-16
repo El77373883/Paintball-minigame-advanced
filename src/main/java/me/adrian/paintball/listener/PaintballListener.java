b4d43cfdc09832dd1fd24280cf5f7715b3959f1f
SoyAdrianYT001 <100530918+El77373883@users.noreply.github.com>
2026-03-16 13:09:01 -0600

    Update PaintballListener.java

--- src/main/java/me/adrian/paintball/listener/PaintballListener.java
+++ src/main/java/me/adrian/paintball/listener/PaintballListener.java
@@ -1,18 +1,27 @@
 package me.adrian.paintball.listener;
 
+import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.ProjectileHitEvent;
-import org.bukkit.entity.Snowball;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Snowball;
 import me.adrian.paintball.game.GameManager;
+import me.adrian.paintball.gui.ShopGUI;
+import me.adrian.paintball.gui.ArenaPanelGUI;
+import me.adrian.paintball.game.Arena;
 
 public class PaintballListener implements Listener {
 
     private final GameManager gameManager;
+    private final ShopGUI shopGUI;
+    private final ArenaPanelGUI panelGUI;
 
-    public PaintballListener(GameManager gameManager) {
-        this.gameManager = gameManager;
+    public PaintballListener(GameManager gm) {
+        this.gameManager = gm;
+        this.shopGUI = new ShopGUI(gm);
+        this.panelGUI = new ArenaPanelGUI();
     }
 
     @EventHandler
@@ -25,5 +34,21 @@ public class PaintballListener implements Listener {
         Player hit = (Player) event.getHitEntity();
 
         gameManager.eliminate(shooter, hit);
+    }
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent e) {
+        if (!(e.getWhoClicked() instanceof Player)) return;
+        Player p = (Player) e.getWhoClicked();
+        e.setCancelled(true);
+
+        String title = e.getView().getTitle();
+
+        if (title.equals("§6Tienda de Paintball")) {
+            shopGUI.handleClick(p, e.getSlot());
+        } else if (title.startsWith("§6Panel Arena: ")) {
+            Arena arena = gameManager.getArena(p);
+            panelGUI.handleClick(p, e.getSlot(), arena);
+        }
     }
 }
