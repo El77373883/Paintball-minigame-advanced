bae59b7b0efc9a6dfa05be0a7a6e1dcf7eb9b1e6
SoyAdrianYT001 <100530918+El77373883@users.noreply.github.com>
2026-03-16 11:26:44 -0600

    Update GameTeam.java

--- src/main/java/me/adrian/paintball/game/GameTeam.java
+++ src/main/java/me/adrian/paintball/game/GameTeam.java
@@ -1,5 +1,18 @@
 package me.adrian.paintball.game;
 
+import org.bukkit.Color;
+
 public enum GameTeam {
-    GREEN, BLUE
+    BLUE(Color.BLUE),
+    GREEN(Color.GREEN);
+
+    private final Color color;
+
+    GameTeam(Color color) {
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
 }
