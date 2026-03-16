9b3ed8b9046f6764c7469e38cb8d77c116492f0f
SoyAdrianYT001 <100530918+El77373883@users.noreply.github.com>
2026-03-16 12:01:06 -0600

    Update PaintballListener.java

--- src/main/java/me/adrian/paintball/listener/PaintballListener.java
+++ src/main/java/me/adrian/paintball/listener/PaintballListener.java
@@ -1,47 +1,33 @@
 package me.adrian.paintball.listener;
 
 import me.adrian.paintball.game.GameManager;
-import me.adrian.paintball.game.GameManager.GameTeam;
+import me.adrian.paintball.gui.ShopGUI;
+import org.bukkit.event.Listener;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Snowball;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
-import org.bukkit.entity.Player;
-import org.bukkit.entity.Snowball;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.entity.ProjectileHitEvent;
 
 public class PaintballListener implements Listener {
 
-    private final GameManager gameManager;
+    private final GameManager gm;
+    private final ShopGUI shop;
 
-    public PaintballListener(GameManager gameManager) {
-        this.gameManager = gameManager;
+    public PaintballListener(GameManager gm) {
+        this.gm = gm;
+        this.shop = new ShopGUI(gm);
     }
 
     @EventHandler
     public void onSnowballHit(ProjectileHitEvent e) {
         if (!(e.getEntity() instanceof Snowball)) return;
-        if (!(e.getHitEntity() instanceof Player)) return;
+        if (!(e.getHitEntity() instanceof Player hit)) return;
+        if (!(e.getEntity().getShooter() instanceof Player shooter)) return;
 
-        Player shooter = (Player) ((Snowball) e.getEntity()).getShooter();
-        Player hit = (Player) e.getHitEntity();
-
-        eliminatePlayer(shooter, hit);
-    }
-
-    public void eliminatePlayer(Player shooter, Player eliminated) {
-        if (shooter == null || eliminated == null) return;
-
-        gameManager.eliminatePlayer(shooter, eliminated);
-
-        Location loc = eliminated.getLocation();
-        var world = loc.getWorld();
-        if (world != null) {
-            world.spawnParticle(Particle.CRIT, loc, 30, 0.5, 0.5, 0.5, 0.1);
-            world.playSound(loc, Sound.ENTITY_LIGHTNING_BOLT_THUNDER, 1.0f, 1.0f);
-            world.strikeLightningEffect(loc);
-        }
+        gm.eliminate(shooter, hit);
     }
 }
