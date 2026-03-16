20d85cf36e659cfb664cfb476e3fd52a748d829a
SoyAdrianYT001 <100530918+El77373883@users.noreply.github.com>
2026-03-16 13:02:56 -0600

    Update Arena.java

--- src/main/java/me/adrian/paintball/game/Arena.java
+++ src/main/java/me/adrian/paintball/game/Arena.java
@@ -1,29 +1,5 @@
 package me.adrian.paintball.game;
 
-import org.bukkit.Location;
-
-public class Arena {
-
-    private final String name;
-    private Location blueSpawn;
-    private Location greenSpawn;
-
-    public Arena(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setBlueSpawn(Location loc) { blueSpawn = loc; }
-    public void setGreenSpawn(Location loc) { greenSpawn = loc; }
-
-    public Location getBlueSpawn() { return blueSpawn; }
-    public Location getGreenSpawn() { return greenSpawn; }
-
-    public GameTeam getTeam(org.bukkit.entity.Player p) {
-        // Default team selection (simple)
-        return GameTeam.BLUE;
-    }
+public enum GameTeam {
+    BLUE, GREEN
 }
