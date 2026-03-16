8c49badd66e594d93731c9ecda73292179a7dd47
SoyAdrianYT001 <100530918+El77373883@users.noreply.github.com>
2026-03-16 15:48:08 -0600

    Update PaintballPlugin.java

--- src/main/java/me/adrian/paintball/PaintballPlugin.java
+++ src/main/java/me/adrian/paintball/PaintballPlugin.java
@@ -6,24 +6,48 @@ import me.adrian.paintball.command.PaintballCommand;
 import me.adrian.paintball.listener.ShopListener;
 import me.adrian.paintball.shop.ShopGUI;
 
-public class PaintballPlugin extends JavaPlugin {
+public final class PaintballPlugin extends JavaPlugin {
+
+    // Instancia estática para poder usar getInstance()
+    private static PaintballPlugin instance;
 
     private GameManager gameManager;
     private ShopGUI shopGUI;
 
     @Override
     public void onEnable() {
+        // Guardamos instancia
+        instance = this;
+
+        // Inicializamos GameManager y ShopGUI
         this.gameManager = new GameManager();
         this.shopGUI = new ShopGUI(gameManager);
 
+        // Registrar comando /pa
         getCommand("pa").setExecutor(new PaintballCommand(this));
+
+        // Registrar listener de la tienda
         getServer().getPluginManager().registerEvents(new ShopListener(shopGUI), this);
+
+        getLogger().info("PaintballPlugin habilitado correctamente.");
     }
 
+    @Override
+    public void onDisable() {
+        getLogger().info("PaintballPlugin deshabilitado.");
+    }
+
+    // Getter estático para obtener la instancia del plugin
+    public static PaintballPlugin getInstance() {
+        return instance;
+    }
+
+    // Getter del GameManager
     public GameManager getGameManager() {
         return gameManager;
     }
 
+    // Getter del ShopGUI
     public ShopGUI getShopGUI() {
         return shopGUI;
     }
