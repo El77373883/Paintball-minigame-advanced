efc48d00879f633b3bb6d8833e3e79a723ab041b
SoyAdrianYT001 <100530918+El77373883@users.noreply.github.com>
2026-03-16 16:53:11 -0600

    Update PaintballAdminCommand.java

--- src/main/java/me/adrian/paintball/command/PaintballAdminCommand.java
+++ src/main/java/me/adrian/paintball/command/PaintballAdminCommand.java
@@ -1,11 +1,13 @@
 package me.adrian.paintball.command;
 
 import me.adrian.paintball.PaintballPlugin;
-import me.adrian.paintball.game.Arena;
 import me.adrian.paintball.game.GameManager;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class PaintballAdminCommand implements CommandExecutor {
 
@@ -20,28 +22,51 @@ public class PaintballAdminCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage(ChatColor.RED + "Solo jugadores pueden usar este comando.");
+            return true;
+        }
+
         if (args.length == 0) {
-            sender.sendMessage("§cUsa: /paadmin arena <crear/nombre>");
+            player.sendMessage(ChatColor.RED + "Usa /paadmin <create|delete|setspawn|start|end|list|reload|setlobby>");
             return true;
         }
 
-        if (args[0].equalsIgnoreCase("arena")) {
-            if (args.length < 2) {
-                sender.sendMessage("§cDebes poner un nombre para la arena.");
+        switch (args[0].toLowerCase()) {
+
+            case "create":
+                if (args.length < 2) {
+                    player.sendMessage(ChatColor.RED + "Usa: /paadmin create <arena>");
+                    return true;
+                }
+                gameManager.createArena(args[1]);
+                player.sendMessage(ChatColor.GREEN + "Arena creada: " + ChatColor.GOLD + args[1]);
                 return true;
-            }
 
-            String name = args[1];
+            case "delete":
+                if (args.length < 2) {
+                    player.sendMessage(ChatColor.RED + "Usa: /paadmin delete <arena>");
+                    return true;
+                }
+                gameManager.getArenas().removeIf(a -> a.getName().equalsIgnoreCase(args[1]));
+                player.sendMessage(ChatColor.YELLOW + "Arena eliminada: " + ChatColor.GOLD + args[1]);
+                return true;
 
-            // ✅ Aquí reemplazamos addArena() por createArena()
-            // Antes: gameManager.addArena(new Arena(name));
-            // Ahora:
-            gameManager.createArena(name);
+            case "list":
+                String list = gameManager.getArenas().stream()
+                        .map(a -> a.getName() + " (" + a.getPlayers().size() + " jugadores)")
+                        .reduce((a1, a2) -> a1 + ", " + a2).orElse("No hay arenas");
+                player.sendMessage(ChatColor.AQUA + "Arenas: " + ChatColor.GREEN + list);
+                return true;
 
-            sender.sendMessage("§aArena creada: " + name);
-            return true;
+            case "reload":
+                plugin.reloadConfig();
+                player.sendMessage(ChatColor.GREEN + "Plugin recargado.");
+                return true;
+
+            default:
+                player.sendMessage(ChatColor.RED + "Comando desconocido. Usa /paadmin create/delete/list/reload");
+                return true;
         }
-
-        return false;
     }
 }
