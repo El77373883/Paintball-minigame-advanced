5ac8ba6316d7911761a51b040c008eb9b7fbca2b
SoyAdrianYT001 <100530918+El77373883@users.noreply.github.com>
2026-03-16 19:29:27 -0600

    Update PaintballPlugin.java

--- src/main/java/me/adrian/paintball/PaintballPlugin.java
+++ src/main/java/me/adrian/paintball/PaintballPlugin.java
@@ -1,51 +1,62 @@
 package me.adrian.paintball;
 
-import me.adrian.paintball.command.PaintballAdminCommand;
 import me.adrian.paintball.command.PaintballCommand;
+import me.adrian.paintball.command.PaintballAdminCommand;
 import me.adrian.paintball.game.GameManager;
 import me.adrian.paintball.listener.AdminGUIListener;
 import me.adrian.paintball.listener.PaintballListener;
 import me.adrian.paintball.listener.ShopListener;
+import me.adrian.paintball.scoreboard.ScoreboardTask;
+import me.adrian.paintball.shop.ShopGUI;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PaintballPlugin extends JavaPlugin {
 
+    private static PaintballPlugin instance; // Instancia estática
     private GameManager gameManager;
+    private ShopGUI shopGUI;
 
     @Override
     public void onEnable() {
 
-        // Config
-        saveDefaultConfig();
+        // Inicializar instancia
+        instance = this;
 
-        // Game Manager
-        gameManager = new GameManager(this);
+        // Inicializar GameManager y ShopGUI
+        this.gameManager = new GameManager();
+        this.shopGUI = new ShopGUI(this.gameManager);
 
-        // Comandos
-        getCommand("pa").setExecutor(new PaintballCommand(this));
-        getCommand("paadmin").setExecutor(new PaintballAdminCommand(this));
+        // Registrar comandos
+        this.getCommand("pa").setExecutor(new PaintballCommand(this));
+        this.getCommand("paadmin").setExecutor(new PaintballAdminCommand(this));
 
-        // Listeners
+        // Registrar listeners
         getServer().getPluginManager().registerEvents(new PaintballListener(this), this);
         getServer().getPluginManager().registerEvents(new ShopListener(this), this);
-
-        // 🔥 ADMIN PANEL LISTENER
         getServer().getPluginManager().registerEvents(new AdminGUIListener(this), this);
 
-        getLogger().info("PaintballAdvanced cargado correctamente.");
-        getLogger().info("Plugin creado por soyadrianyt001");
+        // Iniciar ScoreboardTask cada 40 ticks (2 segundos)
+        new ScoreboardTask(gameManager).runTaskTimer(this, 0, 40);
 
+        getLogger().info("§aPaintballAdvanced habilitado!");
     }
 
     @Override
     public void onDisable() {
+        getLogger().info("§cPaintballAdvanced deshabilitado!");
+    }
 
-        getLogger().info("PaintballAdvanced desactivado.");
-
+    // Getters
+    public static PaintballPlugin getInstance() {
+        return instance;
     }
 
     public GameManager getGameManager() {
         return gameManager;
+    }
+
+    public ShopGUI getShopGUI() {
+        return shopGUI;
     }
 
 }
