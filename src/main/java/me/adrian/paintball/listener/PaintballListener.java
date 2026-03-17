6daf091ae70cb6b0c0a4992f150a9235081a90e3
SoyAdrianYT001 <100530918+El77373883@users.noreply.github.com>
2026-03-16 19:18:55 -0600

    Update PaintballListener.java

--- src/main/java/me/adrian/paintball/listener/PaintballListener.java
+++ src/main/java/me/adrian/paintball/listener/PaintballListener.java
@@ -1,15 +1,12 @@
 package me.adrian.paintball.listener;
 
 import me.adrian.paintball.PaintballPlugin;
-import me.adrian.paintball.game.GameManager;
-import org.bukkit.Bukkit;
-import org.bukkit.Sound;
+import me.adrian.paintball.utils.CoinsManager;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Snowball;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.ProjectileHitEvent;
 
 public class PaintballListener implements Listener {
 
@@ -22,28 +19,32 @@ public class PaintballListener implements Listener {
     @EventHandler
     public void onSnowballHit(EntityDamageByEntityEvent e){
 
-        if(!(e.getDamager() instanceof Snowball)) return;
         if(!(e.getEntity() instanceof Player)) return;
+
+        Player victim = (Player) e.getEntity();
+
+        if(!(e.getDamager() instanceof Snowball)) return;
 
         Snowball ball = (Snowball) e.getDamager();
 
         if(!(ball.getShooter() instanceof Player)) return;
 
-        Player shooter = (Player) ball.getShooter();
-        Player victim = (Player) e.getEntity();
+        Player killer = (Player) ball.getShooter();
 
         e.setCancelled(true);
 
-        victim.setHealth(0);
+        if(victim.equals(killer)) return;
 
-        GameManager gm = plugin.getGameManager();
-
-        gm.giveKill(shooter);
-
+        // efecto trueno
         victim.getWorld().strikeLightningEffect(victim.getLocation());
 
-        shooter.playSound(shooter.getLocation(), Sound.ENTITY_PLAYER_LEVELUP,1,1);
+        // matar jugador
+        victim.setHealth(0);
 
+        // coins
+        CoinsManager.addCoins(killer,3);
+
+        killer.sendMessage("§6+3 coins por eliminación");
     }
 
 }
