1d32633f6e5d547f91b02b7ca390cdf965af3db2
SoyAdrianYT001 <100530918+El77373883@users.noreply.github.com>
2026-03-16 19:05:09 -0600

    Update TeamArmor.java

--- src/main/java/me/adrian/paintball/teams/TeamArmor.java
+++ src/main/java/me/adrian/paintball/teams/TeamArmor.java
@@ -1,1 +1,43 @@
+package me.adrian.paintball.game;
 
+import org.bukkit.Color;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.LeatherArmorMeta;
+
+public class TeamArmor {
+
+    public static void equipBlue(Player p){
+
+        p.getInventory().setHelmet(create(Material.LEATHER_HELMET, Color.BLUE));
+        p.getInventory().setChestplate(create(Material.LEATHER_CHESTPLATE, Color.BLUE));
+        p.getInventory().setLeggings(create(Material.LEATHER_LEGGINGS, Color.BLUE));
+        p.getInventory().setBoots(create(Material.LEATHER_BOOTS, Color.BLUE));
+
+    }
+
+    public static void equipGreen(Player p){
+
+        p.getInventory().setHelmet(create(Material.LEATHER_HELMET, Color.GREEN));
+        p.getInventory().setChestplate(create(Material.LEATHER_CHESTPLATE, Color.GREEN));
+        p.getInventory().setLeggings(create(Material.LEATHER_LEGGINGS, Color.GREEN));
+        p.getInventory().setBoots(create(Material.LEATHER_BOOTS, Color.GREEN));
+
+    }
+
+    private static ItemStack create(Material mat, Color color){
+
+        ItemStack item = new ItemStack(mat);
+
+        LeatherArmorMeta meta = (LeatherArmorMeta) item.getItemMeta();
+
+        meta.setColor(color);
+
+        item.setItemMeta(meta);
+
+        return item;
+
+    }
+
+}
