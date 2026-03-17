2e573dac6c9aaf8e11ddd4208b26d3254bdaa585
SoyAdrianYT001 <100530918+El77373883@users.noreply.github.com>
2026-03-16 19:25:57 -0600

    Update ScoreboardTask.java

--- src/main/java/me/adrian/paintball/scoreboard/ScoreboardTask.java
+++ src/main/java/me/adrian/paintball/scoreboard/ScoreboardTask.java
@@ -1,34 +1,27 @@
 package me.adrian.paintball.scoreboard;
 
+import me.adrian.paintball.game.GameManager;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
-import me.adrian.paintball.PaintballPlugin;
-import me.adrian.paintball.game.GameManager;
 
 public class ScoreboardTask extends BukkitRunnable {
 
-    private final PaintballPlugin plugin;
-    private final GameManager gameManager;
-    private int time = 300; // 5 minutes
+    private GameManager gameManager;
 
-    public ScoreboardTask(PaintballPlugin plugin, GameManager gm) {
-        this.plugin = plugin;
-        this.gameManager = gm;
+    public ScoreboardTask(GameManager gameManager){
+        this.gameManager = gameManager;
     }
 
     @Override
-    public void run() {
-        for (Player p : Bukkit.getOnlinePlayers()) {
-            p.sendActionBar("Kills: "+gameManager.getKills(p)+" | Coins: "+gameManager.getCoins(p)+" | Time: "+time);
+    public void run(){
 
-            // Countdown sounds and titles
-            if(time <=10 && time>0){
-                p.sendTitle("Comienza en", String.valueOf(time), 0, 20,0);
-                p.playSound(p.getLocation(), org.bukkit.Sound.BLOCK_NOTE_BLOCK_PLING,1f,1f);
-            }
+        for(Player p : Bukkit.getOnlinePlayers()){
 
-            if(time>0) time--;
+            ScoreboardManager.update(p,gameManager);
+
         }
+
     }
+
 }
