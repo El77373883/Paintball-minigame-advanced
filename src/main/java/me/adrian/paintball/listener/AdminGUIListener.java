15845011636118b5f51623c411159e17fef8800f
SoyAdrianYT001 <100530918+El77373883@users.noreply.github.com>
2026-03-16 19:07:32 -0600

    Update AdminGUIListener.java

--- src/main/java/me/adrian/paintball/listener/AdminGUIListener.java
+++ src/main/java/me/adrian/paintball/listener/AdminGUIListener.java
@@ -1,20 +1,78 @@
 package me.adrian.paintball.listener;
 
-import me.adrian.paintball.gui.AdminPanelGUI;
-import org.bukkit.event.Listener;
+import me.adrian.paintball.PaintballPlugin;
+import me.adrian.paintball.menu.ArenaPanelGUI;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 
 public class AdminGUIListener implements Listener {
 
-    private final AdminPanelGUI gui;
+    private PaintballPlugin plugin;
 
-    public AdminGUIListener(AdminPanelGUI gui) { this.gui = gui; }
+    public AdminGUIListener(PaintballPlugin plugin){
+        this.plugin = plugin;
+    }
 
     @EventHandler
-    public void onClick(InventoryClickEvent e) {
-        if (e.getView().getTitle().equals("§6Admin Arena Panel")) {
-            gui.handleClick(e);
+    public void click(InventoryClickEvent e){
+
+        if(e.getView().getTitle().equals("§8Panel Admin Paintball")){
+
+            e.setCancelled(true);
+
+            Player p = (Player) e.getWhoClicked();
+
+            if(e.getCurrentItem() == null) return;
+
+            Material mat = e.getCurrentItem().getType();
+
+            if(mat == Material.DIAMOND_SWORD){
+
+                p.closeInventory();
+
+                p.sendMessage("§aEscribe en el chat el nombre de la arena.");
+
+            }
+
+            if(mat == Material.STICK){
+
+                new ArenaPanelGUI().open(p);
+
+            }
+
         }
+
+        if(e.getView().getTitle().equals("§8Editor de Arena")){
+
+            e.setCancelled(true);
+
+            Player p = (Player) e.getWhoClicked();
+
+            if(e.getCurrentItem()==null) return;
+
+            Material mat = e.getCurrentItem().getType();
+
+            if(mat == Material.GOLD_BLOCK){
+
+                plugin.getGameManager().getArena("default").setPos1(p.getLocation());
+
+                p.sendMessage("§aPos1 guardada");
+
+            }
+
+            if(mat == Material.EMERALD_BLOCK){
+
+                plugin.getGameManager().getArena("default").setPos2(p.getLocation());
+
+                p.sendMessage("§aPos2 guardada");
+
+            }
+
+        }
+
     }
+
 }
