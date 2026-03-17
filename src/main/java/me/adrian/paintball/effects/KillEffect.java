f5ffd1fddfdbbbd9a3753f5d0f50efd1c3dab796
SoyAdrianYT001 <100530918+El77373883@users.noreply.github.com>
2026-03-16 19:17:19 -0600

    Update KillEffect.java

--- src/main/java/me/adrian/paintball/effects/KillEffect.java
+++ src/main/java/me/adrian/paintball/effects/KillEffect.java
@@ -1,1 +1,23 @@
+if(e.getEntity() instanceof Player){
 
+    Player victim = (Player) e.getEntity();
+
+    if(e.getDamager() instanceof Snowball){
+
+        Snowball ball = (Snowball) e.getDamager();
+
+        if(ball.getShooter() instanceof Player){
+
+            Player killer = (Player) ball.getShooter();
+
+            victim.getWorld().strikeLightningEffect(victim.getLocation());
+
+            CoinsManager.addCoins(killer,3);
+
+            killer.sendMessage("§6+3 coins");
+
+        }
+
+    }
+
+}
