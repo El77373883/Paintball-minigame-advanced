cbd2e0a9b6405c76c86dcec6375840867bd6e19e
SoyAdrianYT001 <100530918+El77373883@users.noreply.github.com>
2026-03-16 18:56:09 -0600

    Update Arena.java

--- src/main/java/me/adrian/paintball/game/Arena.java
+++ src/main/java/me/adrian/paintball/game/Arena.java
@@ -1,26 +1,70 @@
 package me.adrian.paintball.game;
 
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Arena {
 
-    private final String name;
-    private final Map<Player, GameManager.GameTeam> players = new HashMap<>();
+    private String name;
+    private Location pos1;
+    private Location pos2;
+    private Location spawnBlue;
+    private Location spawnGreen;
+
+    private List<Player> players = new ArrayList<>();
 
     public Arena(String name) {
         this.name = name;
     }
 
-    public String getName() { return name; }
-
-    public Collection<Player> getPlayers() { return players.keySet(); }
-
-    public void addPlayer(Player player, GameManager.GameTeam team) {
-        players.put(player, team);
+    public String getName() {
+        return name;
     }
 
-    public void removePlayer(Player player) {
-        players.remove(player);
+    public void setPos1(Location pos1) {
+        this.pos1 = pos1;
+    }
+
+    public void setPos2(Location pos2) {
+        this.pos2 = pos2;
+    }
+
+    public Location getPos1() {
+        return pos1;
+    }
+
+    public Location getPos2() {
+        return pos2;
+    }
+
+    public void setSpawnBlue(Location loc) {
+        this.spawnBlue = loc;
+    }
+
+    public void setSpawnGreen(Location loc) {
+        this.spawnGreen = loc;
+    }
+
+    public Location getSpawnBlue() {
+        return spawnBlue;
+    }
+
+    public Location getSpawnGreen() {
+        return spawnGreen;
+    }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public void addPlayer(Player p) {
+        players.add(p);
+    }
+
+    public void removePlayer(Player p) {
+        players.remove(p);
     }
 }
