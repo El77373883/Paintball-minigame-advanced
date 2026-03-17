2d123557830f3a37ad98ee1cd9c138f84c882c7e
SoyAdrianYT001 <100530918+El77373883@users.noreply.github.com>
2026-03-16 19:21:08 -0600

    Update Arena.java

--- src/main/java/me/adrian/paintball/game/Arena.java
+++ src/main/java/me/adrian/paintball/game/Arena.java
@@ -1,7 +1,8 @@
 package me.adrian.paintball.game;
 
-import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,62 +10,77 @@ import java.util.List;
 public class Arena {
 
     private String name;
-    private Location pos1;
-    private Location pos2;
-    private Location spawnBlue;
-    private Location spawnGreen;
+    private boolean started;
 
     private List<Player> players = new ArrayList<>();
 
-    public Arena(String name) {
+    public Arena(String name){
         this.name = name;
+        this.started = false;
     }
 
-    public String getName() {
+    public String getName(){
         return name;
     }
 
-    public void setPos1(Location pos1) {
-        this.pos1 = pos1;
+    public boolean isStarted(){
+        return started;
     }
 
-    public void setPos2(Location pos2) {
-        this.pos2 = pos2;
+    public void setStarted(boolean started){
+        this.started = started;
     }
 
-    public Location getPos1() {
-        return pos1;
-    }
-
-    public Location getPos2() {
-        return pos2;
-    }
-
-    public void setSpawnBlue(Location loc) {
-        this.spawnBlue = loc;
-    }
-
-    public void setSpawnGreen(Location loc) {
-        this.spawnGreen = loc;
-    }
-
-    public Location getSpawnBlue() {
-        return spawnBlue;
-    }
-
-    public Location getSpawnGreen() {
-        return spawnGreen;
-    }
-
-    public List<Player> getPlayers() {
+    public List<Player> getPlayers(){
         return players;
     }
 
-    public void addPlayer(Player p) {
+    public void addPlayer(Player p){
+
         players.add(p);
+
+        // limpiar inventario
+        p.getInventory().clear();
+
+        // dar snowballs
+        p.getInventory().addItem(new ItemStack(Material.SNOWBALL,64));
+        p.getInventory().addItem(new ItemStack(Material.SNOWBALL,64));
+
+        // armadura
+        ItemStack helmet = new ItemStack(Material.LEATHER_HELMET);
+        ItemStack chest = new ItemStack(Material.LEATHER_CHESTPLATE);
+        ItemStack legs = new ItemStack(Material.LEATHER_LEGGINGS);
+        ItemStack boots = new ItemStack(Material.LEATHER_BOOTS);
+
+        p.getInventory().setHelmet(helmet);
+        p.getInventory().setChestplate(chest);
+        p.getInventory().setLeggings(legs);
+        p.getInventory().setBoots(boots);
+
+        p.sendMessage("§aEntraste a la arena de Paintball!");
     }
 
-    public void removePlayer(Player p) {
+    public void removePlayer(Player p){
+
         players.remove(p);
+
+        p.getInventory().clear();
+
+        p.sendMessage("§cSaliste de la arena.");
     }
+
+    public void reset(){
+
+        for(Player p : players){
+
+            p.getInventory().clear();
+            p.sendMessage("§eLa partida terminó.");
+
+        }
+
+        players.clear();
+        started = false;
+
+    }
+
 }
