0404c77456ad1d9a3e6cdbc2089143f9ab7cb116
SoyAdrianYT001 <100530918+El77373883@users.noreply.github.com>
2026-03-16 18:57:36 -0600

    Update CoinsManager.java

--- src/main/java/me/adrian/paintball/utils/CoinsManager.java
+++ src/main/java/me/adrian/paintball/utils/CoinsManager.java
@@ -1,25 +1,20 @@
 package me.adrian.paintball.utils;
 
 import org.bukkit.entity.Player;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class CoinsManager {
-    private static final Map<Player, Integer> coins = new HashMap<>();
 
-    public static int getCoins(Player player) {
-        return coins.getOrDefault(player, 0);
+    private Map<Player, Integer> coins = new HashMap<>();
+
+    public int getCoins(Player p) {
+        return coins.getOrDefault(p, 0);
     }
 
-    public static void addCoins(Player player, int amount) {
-        coins.put(player, getCoins(player) + amount);
+    public void addCoins(Player p, int amount) {
+        coins.put(p, getCoins(p) + amount);
     }
 
-    public static boolean removeCoins(Player player, int amount) {
-        if (getCoins(player) >= amount) {
-            coins.put(player, getCoins(player) - amount);
-            return true;
-        }
-        return false;
-    }
 }
